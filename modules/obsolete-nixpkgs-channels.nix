7e700cd136a72c8067908c617ed94742450ce331
Emil Thorsoe <jekarlson@gmail.com>
2023-06-30 09:18:58 +0300

    add module obsolete-nixpkgs-channels
    This module adds to NIX_PATH some obsolete nixos versions of nixpkgs

---
+++ modules/obsolete-nixpkgs-channels.nix
@@ -0,0 +1,20 @@
+{ config, pkgs, lib, ... }: {
+  environment.etc =
+    let
+      oldChans = {
+        "nixos-20.09" = "sha256-tAMJnUwfaDEB2aa31jGcu7R7bzGELM9noc91L2PbVjg=";
+        "nixos-21.05" = "sha256-lkA5X3VNMKirvA+SUzvEhfA7XquWLci+CGi505YFAIs=";
+        "nixos-21.11" = "sha256-hekabNdTdgR/iLsgce5TGWmfIDZ86qjPhxDg/8TlzhE=";
+        "nixos-22.05" = "sha256-Zffu01pONhs/pqH07cjlF10NnMDLok8ix5Uk4rhOnZQ=";
+      };
+      getDir = n: v: {
+        name = "tuxnix/channels/${n}";
+        value.source = pkgs.fetchgit {
+          rev = "refs/heads/${n}";
+          sha256 = v;
+          url = "https://github.com/NixOS/nixpkgs.git";
+        };
+      };
+    in
+    lib.mapAttrs' getDir oldChans;
+}

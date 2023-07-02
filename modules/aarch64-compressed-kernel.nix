af77c51fbcc5d0682e542c6721fdccb0668cf701
Emil Thorsoe <jekarlson@gmail.com>
2023-07-02 17:01:32 +0300

    add aarch64-compressed-kernel module
    This module defines aarch64 hostPlatform that builds kernel with EFI
    generic self decompressing kernel image.

---
+++ modules/aarch64-compressed-kernel.nix
@@ -0,0 +1,45 @@
+{ lib, ... }: {
+  boot.kernelPatches = [
+    {
+      name = "config";
+      patch = null;
+      extraStructuredConfig = { EFI_ZBOOT = lib.kernel.yes; KERNEL_ZSTD = lib.kernel.yes; };
+    }
+  ];
+  nixpkgs.hostPlatform = {
+    system = "aarch64-linux";
+    linux-kernel = {
+      name = "aarch64-multiplatform";
+      baseConfig = "defconfig";
+      DTB = true;
+      autoModules = true;
+      preferBuiltin = true;
+      extraConfig = ''
+        # Raspberry Pi 3 stuff. Not needed for   s >= 4.10.
+        ARCH_BCM2835 y
+        BCM2835_MBOX y
+        BCM2835_WDT y
+        RASPBERRYPI_FIRMWARE y
+        RASPBERRYPI_POWER y
+        SERIAL_8250_BCM2835AUX y
+        SERIAL_8250_EXTENDED y
+        SERIAL_8250_SHARE_IRQ y
+
+        # Cavium ThunderX stuff.
+        PCI_HOST_THUNDER_ECAM y
+
+        # Nvidia Tegra stuff.
+        PCI_TEGRA y
+
+        # The default (=y) forces us to have the XHCI firmware available in initrd,
+        # which our initrd builder can't currently do easily.
+        USB_XHCI_TEGRA m
+      '';
+      target = "vmlinuz.efi";
+      installTarget = "zinstall";
+    };
+    gcc = {
+      arch = "armv8-a";
+    };
+  };
+}

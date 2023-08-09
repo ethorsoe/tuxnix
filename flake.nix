6b24d707e0c7a503d73e06df020f25a8ba8e08a3
Emil Thorsoe <jekarlson@gmail.com>
2023-08-09 12:56:56 +0300

    support sets of modules
    Add new parameter to tuxnixSystem attrsOf list that define the sets.
    Refer to sets with prefix "set:" in modules, also sets can refer to
    other sets this way.
    
    Change-Id: Ie577870c351b8d1cf546a8aa4d277bd477cc72ea

--- flake.nix
+++ flake.nix
@@ -7,53 +7,90 @@
 
   outputs = { nixpkgs, self, ... }@flakeInputs:
     {
-      tuxnixSystem = { inputattrs, system, modules, modulesPaths ? [ ], extraArgs ? { } }:
+      tuxnixSystem =
+        { inputattrs
+        , sets ? { }
+        , system
+        , modules
+        , modulesPaths ? [ ]
+        , extraArgs ? { }
+        }:
         let
           allChannels = flakeInputs // inputattrs;
           lib = nixpkgs.lib;
           tlib = import ./lib.nix lib;
           effectiveModulesPaths = [ ./modules ] ++ modulesPaths;
-          foldModule = mods: newmod: mods ++
-            (if builtins.isAttrs newmod || builtins.isFunction newmod || builtins.isPath newmod
-            then [ newmod ]
-            else if builtins.isString newmod
-            then tlib.resolveModule effectiveModulesPaths newmod
-            else throw "Module type not recognized!");
+          handleSetDeps = { handledSets ? { }, handledModules ? { }, passthrus ? [ ], inputs }:
+            let
+              hInputs = builtins.head inputs;
+              isPassthru = !(builtins.isString hInputs);
+              isSet = !isPassthru && lib.hasPrefix "set:" hInputs;
+              isModule = !(isPassthru || isSet);
+              name = lib.removePrefix "set:" hInputs;
+              newHandledSets = handledSets // lib.optionalAttrs isSet { "${name}" = [ ]; };
+              newHandledModules = handledModules // lib.optionalAttrs isModule {
+                "${name}" = [ ];
+              };
+              passthruModule =
+                if builtins.isAttrs hInputs ||
+                  builtins.isFunction hInputs ||
+                  builtins.isPath hInputs
+                then hInputs
+                else throw "Module type not recognized!";
+              newPassthrus = passthrus
+                ++ lib.optional isPassthru passthruModule
+                ++ lib.optionals isModule (tlib.resolveModule effectiveModulesPaths name);
+              newInputs = builtins.tail inputs
+                ++ lib.optionals isSet (handledSets.${name} or sets.${name})
+                ++ lib.optionals isModule (handledModules.${name} or [ name ]);
+            in
+            if [ ] == inputs
+            then passthrus
+            else
+              handleSetDeps {
+                handledSets = newHandledSets;
+                handledModules = newHandledModules;
+                passthrus = newPassthrus;
+                inputs = newInputs;
+              };
           instantiationconfig = inputattrs.instantiationdata.nixosConfig or { };
         in
         nixpkgs.lib.nixosSystem ({
           inherit system;
-          modules = lib.foldl' foldModule [
-            instantiationconfig
+          modules = handleSetDeps
             {
-              environment.etc = nixpkgs.lib.mapAttrs'
-                (n: v: { name = "tuxnix/channels/${n}"; value = { source = v; }; })
-                allChannels;
-              nix.nixPath = [ "/etc/tuxnix/channels" ];
-            }
-            {
-              options.tuxnix.container.modulesPaths = lib.mkOption {
-                default = effectiveModulesPaths;
-                description = "Extra paths to load modules from.";
-                type = lib.types.listOf lib.types.path;
-              };
-            }
-            {
-              options.tuxnix.update-system = {
-                selfFlakeFilePath = lib.mkOption {
-                  default = inputattrs.self + "/flake.nix";
-                  description = "Path of the flake.nix of top level flake";
-                  type = lib.types.path;
-                };
-                selfFlakePath = lib.mkOption {
-                  default = "./.";
-                  description = "Path of the top level flake";
-                  type = lib.types.str;
-                };
-              };
-            }
-          ]
-            modules;
+              passthrus = [
+                instantiationconfig
+                {
+                  environment.etc = nixpkgs.lib.mapAttrs'
+                    (n: v: { name = "tuxnix/channels/${n}"; value = { source = v; }; })
+                    allChannels;
+                  nix.nixPath = [ "/etc/tuxnix/channels" ];
+                }
+                {
+                  options.tuxnix.container.modulesPaths = lib.mkOption {
+                    default = effectiveModulesPaths;
+                    description = "Extra paths to load modules from.";
+                    type = lib.types.listOf lib.types.path;
+                  };
+                }
+                {
+                  options.tuxnix.update-system = {
+                    selfFlakeFilePath = lib.mkOption {
+                      default = inputattrs.self + "/flake.nix";
+                      description = "Path of the flake.nix of top level flake";
+                      type = lib.types.path;
+                    };
+                    selfFlakePath = lib.mkOption {
+                      default = "./.";
+                      description = "Path of the top level flake";
+                      type = lib.types.str;
+                    };
+                  };
+                }
+              ];
+              inputs = modules;
+            };
           specialArgs = { inputattrs = allChannels; };
         } // extraArgs);
     };

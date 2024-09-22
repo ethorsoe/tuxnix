58cff0ce84a4aa2428bb2e448e74ca0b629355cd
Emil Thorsoe <jekarlson@gmail.com>
2024-09-22 21:29:45 +0300

    modules/writeback-post-build-hook: make flush async
    When running builds directly as root, this frequently avoids
    too many open files issues.
    
    Change-Id: Idb5707e5c3ba2ef111f27d7c30ef97e89bbcc027

--- modules/writeback-post-build-hook.nix
+++ modules/writeback-post-build-hook.nix
@@ -1,7 +1,10 @@
 { pkgs, ... }:
 {
   nix.settings.post-build-hook = pkgs.writeScript "writeback-post-build-hook" ''
-    find $OUT_PATHS "$DRV_PATH" -type f \
+    ${pkgs.systemd}/bin/systemd-run \
+      -u "writeback-post-build-hook@$(systemd-escape "$DRV_PATH")" --no-block \
+      --description="Initiate writeback on $DRV_PATH outputs" \
+      find $OUT_PATHS "$DRV_PATH" -type f \
       -exec ${pkgs.libxfs.bin}/bin/xfs_io -r -c  'sync_range -w 0 0' '{}' +
   '';
 }

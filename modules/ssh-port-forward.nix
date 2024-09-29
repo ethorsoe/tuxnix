2d90d34ab450a9ff1ec7c8ceed95b717dfeb325b
Emil Thorsoe <jekarlson@gmail.com>
2024-09-29 18:00:01 +0300

    add module ssh-port-forward.nix
    This module forwards remote host port to local port 22.
    
    Change-Id: Iceb9b78dbb9402865f6f76c5daa0a50998398e04

---
+++ modules/ssh-port-forward.nix
@@ -0,0 +1,28 @@
+{ config, pkgs, lib, ... }: {
+  options.tuxnix.sshPortForward = {
+    port = lib.mkOption {
+      description = "Port on remote host to be forwarded.";
+      type = lib.types.int;
+    };
+    username = lib.mkOption {
+      description = "Remote host username.";
+      type = lib.types.str;
+    };
+  };
+  config.systemd.services.port-forward = {
+    after = [ "network-online.target" ];
+    description = "Port forward service";
+    serviceConfig = {
+      DynamicUser = true;
+      ExecStart = "${pkgs.openssh}/bin/ssh -i \${CREDENTIALS_DIRECTORY}/key" +
+        " -N -R ${builtins.toString config.tuxnix.sshPortForward.port}:localhost:22" +
+        " ${config.tuxnix.sshPortForward.username}@lohtuchai.dy.fi";
+      LoadCredential = "key:/mnt/persist/root-ssh/id_ed25519";
+      Restart = "always";
+      RestartSec = 120;
+      Type = "simple";
+    };
+    wantedBy = [ "multi-user.target" ];
+    wants = [ "network-online.target" ];
+  };
+}

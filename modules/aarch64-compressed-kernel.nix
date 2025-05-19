bb3f5c7feff64c9ddd7d633bb92ef08124de5d4e
Emil Thorsoe <jekarlson@gmail.com>
2025-05-19 20:59:05 +0300

    modules/aarch64-compressed-kernel: dynamically overlay platform changes
    Do not hardcode old hostPlatform, use current platform obtained via lib.
    
    Change-Id: I23c6c97c01336a888217e5fc2ecab64b60af0eac

--- modules/aarch64-compressed-kernel.nix
+++ modules/aarch64-compressed-kernel.nix
@@ -5,48 +5,11 @@
       patch = null;
       extraStructuredConfig = { EFI_ZBOOT = lib.kernel.yes; KERNEL_ZSTD = lib.kernel.yes; };
     }
-    {
-      name = "hexdump-from-nixos";
-      patch = pkgs.substituteAll {
-        src = ../lib/patches/zboot-hexdump-from-nixos.diff;
-        hexdumpFromNixos = "${pkgs.util-linux}/bin/hexdump";
-      };
-    }
   ];
-  nixpkgs.hostPlatform = {
-    system = "aarch64-linux";
-    linux-kernel = {
-      name = "aarch64-multiplatform";
-      baseConfig = "defconfig";
-      DTB = true;
-      autoModules = true;
-      preferBuiltin = true;
-      extraConfig = ''
-        # Raspberry Pi 3 stuff. Not needed for   s >= 4.10.
-        ARCH_BCM2835 y
-        BCM2835_MBOX y
-        BCM2835_WDT y
-        RASPBERRYPI_FIRMWARE y
-        RASPBERRYPI_POWER y
-        SERIAL_8250_BCM2835AUX y
-        SERIAL_8250_EXTENDED y
-        SERIAL_8250_SHARE_IRQ y
-
-        # Cavium ThunderX stuff.
-        PCI_HOST_THUNDER_ECAM y
-
-        # Nvidia Tegra stuff.
-        PCI_TEGRA y
-
-        # The default (=y) forces us to have the XHCI firmware available in initrd,
-        # which our initrd builder can't currently do easily.
-        USB_XHCI_TEGRA m
-      '';
-      target = "vmlinuz.efi";
-      installTarget = "zinstall";
+  nixpkgs.hostPlatform = lib.recursiveUpdate
+    (lib.systems.elaborate lib.systems.examples.aarch64-multiplatform)
+    {
+      linux-kernel.target = "vmlinuz.efi";
+      linux-kernel.installTarget = "zinstall";
     };
-    gcc = {
-      arch = "armv8-a";
-    };
-  };
 }
